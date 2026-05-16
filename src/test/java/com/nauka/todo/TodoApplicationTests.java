3d1e0de207c97bcf2d9ab082695967121f2b333d
Piotreq11 <p.gorecki1111@wp.pl>
2026-05-16 17:27:01 +0200

    Initial commit

---
+++ src/test/java/com/nauka/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nauka.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
